fe8b0e76a1b0b44c6b5572962819faac74ce382d
sbjonesuk <sam@sbjones.co.uk>
2026-06-25 16:57:15 +0100

    Removed the section library

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use App\Providers\SectionLibraryServiceProvider;
 
 return [
     AppServiceProvider::class,
-    SectionLibraryServiceProvider::class,
 ];
